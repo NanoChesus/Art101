30880b739992e8394e31539e4a92a996d5d62df5
NanoChesus <165968267+NanoChesus@users.noreply.github.com>
2024-06-07 01:00:29 -0700

    push 1
    pushed

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -35,25 +35,24 @@
     // })
 
 
-    $.ajax({
-        url: "https://xkcd.com/info.0.json",
-        data: {},
-        type: "GET",
-        dataType: "json",
-        success: function(data) {
-            console.log(data);
+    const URL = "https://xkcd.com/info.0.json";
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
     
-            var printableDate = "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+    $("#activate").click(function() {
+        $.ajax({
+            url: CORS_PROXY + URL,
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+                console.log(data);
     
-            $("#title").html(data.title);
-            $("#output").append("<img src='" + data.img + "'>");
-            $("#output").append("<p>" + data.alt + "</p>"); 
-        },
-        error: function (jqXHR, textStatus, errorThrown) { 
-            console.log("Error:", textStatus, errorThrown);
-        }
+                $("#title").html(data.title);
+                $("#output").append("<img src='" + data.img + "'>");
+                $("#output").append("<p>" + data.alt + "</p>"); 
+            },
+            error: function (jqXHR, textStatus, errorThrown) { 
+                console.log("Error:", textStatus, errorThrown);
+            }
+        });
     });
     
-
-
-
